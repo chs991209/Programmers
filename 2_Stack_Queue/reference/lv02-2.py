2102e8e398db74898b2613ef0ac700943ecd969e
Euimin Chung <jane3079@korea.ac.kr>
2021-04-21 22:16:27 +0900

    2_Stack_Queue lv02-2 reference

--- 2_Stack_Queue/reference/lv02-2.py
+++ 2_Stack_Queue/reference/lv02-2.py
@@ -1,10 +1,31 @@
+# Brute Force
+
 def solution(prices):
-    answer =[0] * len(prices)
+    answer = [0] * len(prices)
     for i in range(len(prices)):
-        for j in range(i+1, len(prices)):
-            if prices[i] <= prices[j]:
-                answer[i]+=1
-            else:
-                answer[i]+=1
+        for j in range(i + 1, len(prices)):
+            if prices[j] < prices[i]:       # 하락했을시
+                answer[i] = answer[i] + 1
                 break
+            else:
+                answer[i] = answer[i] + 1
+    return answer
+
+# Stack
+
+from collections import deque
+
+def solution(prices):
+    answer = []
+    prices = deque(prices)
+    while prices:
+        i = prices.popleft()
+        count = 0
+        for j in prices:
+            if j < i:               # 하락했을시
+                count = count + 1
+                break
+            else:
+                count = count + 1
+        answer.append(count)
     return answer
