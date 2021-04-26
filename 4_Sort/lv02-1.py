c15abe755df08acdc7216f9427a83759f418b2d2
Euimin Chung <jane3079@korea.ac.kr>
2021-04-26 23:14:52 +0900

    4_Sort lv02-1 가장 큰 수 done

--- 4_Sort/lv02-1.py
+++ 4_Sort/lv02-1.py
@@ -1,10 +1,7 @@
 def solution(numbers):
     numbers = [str(int) for int in numbers]
-
-    numbers = list(reversed(sorted(numbers)))
+    numbers = list(reversed(sorted(numbers, key = lambda x : x*3)))
     numbers = "".join(numbers)
     numbers = int(numbers)
     numbers = str(numbers)
     return numbers
-
-print(solution([3, 30, 310, 4]))
