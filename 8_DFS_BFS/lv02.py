fe4cbf1fe9320f861e676c9d8d5b5af14c8d986b
Euimin Chung <jane3079@korea.ac.kr>
2021-04-30 21:51:07 +0900

    edit lv02 타겟넘버

--- 8_DFS_BFS/lv02.py
+++ 8_DFS_BFS/lv02.py
@@ -1,27 +1,16 @@
-def dfs(numbers, target, depth) :
-    numsum = 0
-    if depth < len(numbers) :
-        count = count + dfs(numbers, target, depth + 1)
-        numbers[depth] = -1 * numbers[depth]
-        count = count + dfs(numbers, target, depth + 1)
-        return count
-    elif depth == len(numbers) :
-        if sum(numbers) == target :
-            return 1
-        return 0
-
-def dfs2(numbers, target, depth, ssum):
-    if depth == len(numbers):
-        if ssum == target:
-            return 1
-        return 0
+def dfs(numbers, target, depth, numsum):
     count = 0
-    count += dfs2(numbers, target, depth + 1, ssum + numbers[depth])
-    count += dfs2(numbers, target, depth + 1, ssum - numbers[depth])
+    if depth < len(numbers):
+        count += dfs(numbers, target, depth + 1, numsum + numbers[depth])
+        count += dfs(numbers, target, depth + 1, numsum - numbers[depth])
+    elif depth == len(numbers):
+        if numsum == target:
+            return 1
+        return 0
     return count
 
 def solution(numbers, target) :
-    answer = dfs2(numbers, target, 0, 0)
+    answer = dfs(numbers, target, 0, 0)
     return answer
 
 
--- 8_DFS_BFS/reference/lv02.py
+++ 8_DFS_BFS/reference/lv02.py
@@ -1,3 +1,5 @@
+#1. 아직 완전히 이해는 못함
+
 def solution(numbers, target) :
     tree = [0]
     for i in numbers :
@@ -9,3 +11,22 @@ def solution(numbers, target) :
     return tree.count(target)
 
 # 출처: https://train-validation-test.tistory.com/entry/Programmers-level-2-%ED%83%80%EA%B2%9F-%EB%84%98%EB%B2%84-python
+
+#2. numbers 리스트를 변화시키는 방법
+def dfs(numbers, target, depth) :
+    count = 0
+    if depth < len(numbers) :
+        count = count + dfs(numbers, target, depth + 1)
+        numbers[depth] = -1 * numbers[depth]
+        count = count + dfs(numbers, target, depth + 1)
+        return count
+    elif depth == len(numbers) :
+        if sum(numbers) == target :
+            return 1
+        return 0
+
+def solution(numbers, target) :
+    answer = dfs(numbers, target, 0)
+    return answer
+
+# 출처: ..
