f2d050e235f82b5c44c38c63c16959ab45a557b6
Euimin Chung <jane3079@korea.ac.kr>
2021-05-05 22:06:58 +0900

    6_Greedy lv02-2 2 out of 12

--- 6_Greedy/lv02-2.py
+++ 6_Greedy/lv02-2.py
@@ -1,9 +1,12 @@
 def solution(number, k):
     number = list(map(int, number))
-    number.sort()
-    number = number[k:len(number) + 1]
-    return number
+    number_s = sorted(number)
+    small = number_s[0:k]
+    answer = [i for i in number if not i in small or small.remove(i)]
+    answer = [str(int) for int in answer]
+    answer = "".join(answer)
+    return answer
 
-number = "1924"
-k = 2
+number = "1231234"
+k = 3
 print(solution(number, k))
