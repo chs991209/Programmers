243c0a510c307a5dd8933090ec1b93c12f19fdc6
Euimin Chung <jane3079@korea.ac.kr>
2021-05-08 18:00:45 +0900

    2021 intern test03 Z도 성공 근데 5/30... 뭐틀린지모름

--- 00_Actual_Test/2021intern/lv03cmd.py
+++ 00_Actual_Test/2021intern/lv03cmd.py
@@ -1,11 +1,12 @@
 def solution(n, k, cmd):
     answer = ['O'] * n
-    #name = []
-    #for nam in range(0, n) :
-    #    name.append(nam)
-    name = ['무지', '콘', '어피치', '제이지', '프로도', '네오', '튜브', '라이언']
+    name = []
+    for nam in range(0, n) :
+        name.append(nam)
+    #name = ['무지', '콘', '어피치', '제이지', '프로도', '네오', '튜브', '라이언']
     select_ = name[k]
-    delete_ = name[k]
+    delete_ = []
+    delete_i = []
     for i in range(0, len(cmd)) :
         if cmd[i][0] == 'U' :
             num = int(cmd[i][2])
@@ -23,25 +24,35 @@ def solution(n, k, cmd):
 
         elif cmd[i][0] == 'C' :
             j = name.index(select_)
-            delete_ = select_
-            answer[j] = 'X'
             if j == len(name)-1 :
                 select_ = name[j]
+                delete_.append(select_)
+                delete_i.append(j)
+                answer[j] = 'X'
                 name.remove(select_)
                 select_ = name[-1]
                 print("c max name= ", select_)
                 print("c max index= ", name.index(select_))
             else :
                 select_ = name[j]
+                delete_.append(select_)
+                delete_i.append(j)
+                answer[j] = 'X'
                 name.remove(select_)
                 select_ = name[j]
                 print("c name= ", select_)
                 print("c index= ", name.index(select_))
 
         elif cmd[i][0] == 'Z' :
-            pass 
+            j = name.index(select_)
+            zname = delete_.pop(-1) # name
+            zidx = delete_i.pop(-1) # index
+            name1 = name[0:zidx]
+            name2 = name[zidx:]
+            name = name1 + [zname] + name2
+            answer[zidx] = 'O'
     answer = "".join(answer)
     answer = str(answer)
     return answer
 
-print(solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2"])) #,"Z","Z"]))
+print(solution(8, 2,["D 2","C","U 3","C","D 4","C","U 2","Z","Z"]))
