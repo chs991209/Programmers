d8806c71231b61840deffeaafb650c046298ad60
Euimin Chung <jane3079@korea.ac.kr>
2021-05-10 20:14:27 +0900

    풍선터트리기, DP

---
+++ 00_Actual_Test/codechal/balloon.py
@@ -0,0 +1,33 @@
+def solution(a):
+    n = len(a)
+    left = [0] * n
+    right = [0] * n
+   
+    small = a[0]
+    for i in range(0, n) :
+        if a[i] <= small :
+            small = a[i]
+            left[i] = small
+        else :
+            left[i] = small
+    
+    small = a[-1]
+    for i in reversed(range(n)):
+        if a[i] <= small :
+            small = a[i]
+            right[i] = small
+        else :
+            right[i] = small
+    
+    count = 0
+    for i in range(n):
+        if i == 0 or i == n - 1:
+            count += 1
+            continue
+        if a[i] < left[i-1] or a[i] < right[i+1] :
+            count += 1
+    return count
+
+a = [-16,27,65,-2,58,-92,-71,-68,-61,-33]
+
+print(solution(a))
