2910d1f7d42e4dbbbdf1c3c221ae186c0b4b05d1
Chs_Mactaengoo <82556127+chs991209@users.noreply.github.com>
2022-01-27 12:02:30 +0900

    2021intern/lv01nums.py
    mutable list에 class type이 통일된 값을 append
    >> comprehension list 할당 proccess 제거, answer 재할당 이전 list의 memory  remove
    return >> obj ->  list answer call>> str class의 method join call >> int() implementation
    # 가독성 고려 modification >> answer_joined = "".join(submit); answer_joined_toint = int(joined_submit)
    # return answer_joined_toint

--- 00_Actual_Test/2021intern/lv01nums.py
+++ 00_Actual_Test/2021intern/lv01nums.py
@@ -1,42 +1,43 @@
 def solution(s):
     answer = []
     i = 0
-    while i < len(s) :
-        if s[i] == 'z':
-            answer.append(0)
-            i += 4
-        elif s[i] == 'o':
-            answer.append(1)
-            i += 3
-        elif s[i] == 't' and s[i+1] == 'w':
-            answer.append(2)
-            i += 3
-        elif s[i] == 't' and s[i+1] == 'h':
-            answer.append(3)
-            i += 5
-        elif s[i] == 'f' and s[i+1] == 'o':
-            answer.append(4)
-            i += 4
-        elif s[i] == 'f' and s[i+1] == 'i':
-            answer.append(5)
-            i += 4
-        elif s[i] == 's' and s[i+1] == 'i':
-            answer.append(6)
-            i += 3
-        elif s[i] == 's' and s[i+1] == 'e':
-            answer.append(7)
-            i += 5
-        elif s[i] == 'e' :
-            answer.append(8)
-            i += 5
-        elif s[i] == 'n' :
-            answer.append(9)
-            i += 4
-        elif '0' <= s[i] <= '9' :
+    while i < len(s):
+        if "0" <= s[i] <= "9":
             answer.append(s[i])
             i += 1
-    answer = [str(int) for int in answer]
-    answer = "".join(answer)
-    answer = int(answer)
-    return answer
+        elif s[i] == "z":
+            answer.append("0")
+            i += 4            
+        elif s[i] == "o":
+            answer.append("1")
+            i += 3            
+        elif s[i] == "t":
+            if s[i + 1] == "w":
+                answer.append("2")
+                i += 3
+            elif s[i + 1] == "h":
+                answer.append("3")
+                i += 5                
+        elif s[i] == "f":
+            if s[i + 1] == "o":
+                answer.append("4")
+                i += 4
+            if s[i + 1] == "i":
+                answer.append("5")
+                i += 4                
+        elif s[i] == "s":
+            if s[i + 1] == "i":
+                answer.append("6")
+                i += 3
+            elif s[i + 1] == "e":
+                answer.append("7")
+                i += 5                
+        elif s[i] == "e" and s[i + 1] == "i":
+            answer.append("8")
+            i += 5            
+        elif s[i] == "n":
+            answer.append("9")
+            i += 4
+    joinedtoint_answer = int("".join(answer))  
+    return joinedtoint_answer
 print(solution("threezero123one"))
